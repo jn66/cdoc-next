8f5566d4d6512df095300ef3d59da086f73e9b71
Johny <5820450+jn66@users.noreply.github.com>
2019-09-06 17:11:16 +0800

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,3 @@
+import '../static/css/tailwind.css'
+
+export default ()=> <div className="font-bold">这是关于页面</div>
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout'
+
+export default () => (
+  <Layout>
+    <h1 className="text-5xl font-bold text-gray-900">Hello CNKI!</h1>
+  </Layout>
+)
